4ed7a1178ba4afdec591856a3db916e29c622176
John Dunnicliffe <dunnicli@gmail.com>
2022-07-19 21:34:51 -0500

    formatting

--- src/lib/title.svelte
+++ src/lib/title.svelte
@@ -1,26 +1,26 @@
 <script>
-	import '../app.css';
-	export let title = 'STX Micro-Stacks';
+  import "../app.css";
+  export let title = "Svelte Micro-Stacks";
 </script>
 
 <div class="title">
-	<a href="/">
-		<img src="/logo1.svg" alt="site logo" />
-	</a>
-	<h1 class="text-3xl font-bold underline">{title}</h1>
+  <a href="/">
+    <img src="/logo1.svg" alt="site logo" />
+  </a>
+  <h1 class="text-3xl font-bold underline">{title}</h1>
 </div>
 
 <style>
-	.title {
-		display: flex;
-		align-items: center;
-	}
+  .title {
+    display: flex;
+    align-items: center;
+  }
 
-	.title img {
-		display: inline-block;
-	}
-	.title h1 {
-		margin-left: 20px;
-		color: deepskyblue;
-	}
+  .title img {
+    display: inline-block;
+  }
+  .title h1 {
+    margin-left: 20px;
+    color: deepskyblue;
+  }
 </style>
