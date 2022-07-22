92da8d90afba474fb12d7535850adad7f76931a1
John Dunnicliffe <dunnicli@gmail.com>
2022-07-22 14:08:46 -0500

    mintNft page is working

--- src/routes/mintNft.svelte
+++ src/routes/mintNft.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
   import { getOpenContractCall } from "@micro-stacks/svelte";
   import { getAccount, getAuth } from "@micro-stacks/svelte";
   import {
@@ -20,6 +20,11 @@
     FungibleConditionCode,
   } from "micro-stacks/transactions";
 
+  import { geNetwork } from "@micro-stacks/svelte";
+
+  const network = geNetwork();
+  $network.setNetwork("testnet");
+
   $: sessionStatus = !$account.stxAddress
     ? "No active session"
     : $account.stxAddress;
@@ -31,42 +36,6 @@
   let recipient = "ST12H4ANQQ2NGN96KB0ZYVDG02NWT99A9TPE22SP9";
   let metadataUri = "";
 
-  const clarityRecipient = standardPrincipalCV(recipient);
-  const clarityUri = stringAsciiCV(metadataUri);
-
-  const functionArgs = [clarityRecipient, clarityUri];
-
-  // contract address for the asset contract
-  const assetAddress = "ST12H4ANQQ2NGN96KB0ZYVDG02NWT99A9TPE22SP9";
-  const postConditionAddress = $account.stxAddress!;
-  const nftPostConditionCode = NonFungibleConditionCode.Owns;
-  const assetContractName = "acatv4";
-  const assetName = "acat";
-  const tokenAssetName = stringAsciiCV("acat");
-
-  const nonFungibleAssetInfo = createAssetInfo(
-    assetAddress,
-    assetContractName,
-    assetName
-  );
-
-  const stxConditionCode = FungibleConditionCode.LessEqual;
-  const stxConditionAmount = 100000000; // denoted in microstacks
-
-  const postConditions = [
-    makeStandardNonFungiblePostCondition(
-      postConditionAddress,
-      nftPostConditionCode,
-      nonFungibleAssetInfo,
-      tokenAssetName
-    ),
-    makeStandardSTXPostCondition(
-      postConditionAddress,
-      stxConditionCode,
-      stxConditionAmount
-    ),
-  ];
-
   /**
   const postConditions = [
     makeStandardSTXPostCondition(
@@ -77,7 +46,43 @@
   ];
   */
 
-  const onClick = async () => {
+  async function onClick() {
+    const clarityRecipient = standardPrincipalCV(recipient);
+    const clarityUri = stringAsciiCV(metadataUri);
+
+    const functionArgs = [clarityRecipient, clarityUri];
+
+    // contract address for the asset contract
+    const assetAddress = "ST12H4ANQQ2NGN96KB0ZYVDG02NWT99A9TPE22SP9";
+    const postConditionAddress = "ST12H4ANQQ2NGN96KB0ZYVDG02NWT99A9TPE22SP9";
+    const nftPostConditionCode = NonFungibleConditionCode.Owns;
+    const assetContractName = "acatv4";
+    const assetName = "acat";
+    const tokenAssetName = stringAsciiCV("acat");
+
+    const nonFungibleAssetInfo = createAssetInfo(
+      assetAddress,
+      assetContractName,
+      assetName
+    );
+
+    const stxConditionCode = FungibleConditionCode.LessEqual;
+    const stxConditionAmount = 100000000; // denoted in microstacks
+
+    const postConditions = [
+      makeStandardNonFungiblePostCondition(
+        postConditionAddress,
+        nftPostConditionCode,
+        nonFungibleAssetInfo,
+        tokenAssetName
+      ),
+      makeStandardSTXPostCondition(
+        postConditionAddress,
+        stxConditionCode,
+        stxConditionAmount
+      ),
+    ];
+
     await $contractCall.openContractCall({
       contractAddress: assetAddress,
       contractName: "acatv4",
@@ -93,7 +98,7 @@
         console.log("popup closed!");
       },
     });
-  };
+  }
 </script>
 
 <svelte:head>
@@ -127,13 +132,15 @@
 
     <br />
     <button
-      {onClick}
-      class="font-bold mt-4 bg-green-500 text-white rounded p-4 shadow-lg"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      on:click={onClick}
     >
-      Just Mint Create Stx NFT
+      Mint STX NFT
     </button>
+
     <p>&nbsp;</p>
     <p>&nbsp;</p>
+
     <h1>TXID:</h1>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
